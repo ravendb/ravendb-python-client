9fd8d741e2f4ce63712b6ab25f21a9c0395a29ed
IdanHaim <haimdude@gmail.com>
2016-02-17 17:40:38 +0200

    files add

---
+++ data/patches.py
@@ -0,0 +1,9 @@
+class ScriptedPatchRequest(object):
+    def __init__(self, script, values=None):
+        if values is None:
+            values = {}
+        self.script = script
+        self.values = values
+
+    def to_json(self):
+        return {"Script": self.script, "Values": self.values}
