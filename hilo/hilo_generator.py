15c8f1c8a2234392dd0de9e5e77926e67340171a
IdanHaim <haimdude@gmail.com>
2016-02-17 17:05:49 +0200

    add lock when try to read from hilo_type dict

--- hilo/hilo_generator.py
+++ hilo/hilo_generator.py
@@ -1,3 +1,4 @@
+from threading import Lock
 
 
 class Range(object):
@@ -11,6 +12,7 @@ class HiloGenerator(object):
     def __init__(self, capacity):
         self.capacity = capacity
         self.hilo_type = {}
+        self.lock = Lock()
 
     def generate_document_id(self, entity, convention, request_handler):
         """
@@ -26,10 +28,12 @@ class HiloGenerator(object):
         name = entity.__class__.__name__
         type_tag_name = convention.default_transform_type_tag_name(entity.__class__.__name__)
         if name in self.hilo_type:
-            self.hilo_type[name].current += 1
+            with self.lock:
+                self.hilo_type[name].current += 1
             if not self.hilo_type[name].current > self.hilo_type[name].max_id:
                 return "{0}/{1}".format(type_tag_name, str(self.hilo_type[name].current)), False
 
         max_id_from_server = request_handler.call_hilo(type_tag_name)
-        self.hilo_type[name] = Range(max_id_from_server + 1, max_id_from_server + convention.max_ids_to_catch)
+        with self.lock:
+            self.hilo_type[name] = Range(max_id_from_server + 1, max_id_from_server + convention.max_ids_to_catch)
         return "{0}/{1}".format(type_tag_name, str(self.hilo_type[name].current)), True
