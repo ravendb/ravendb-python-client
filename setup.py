b1cf46f1429db277028b50b0ec4638b95869dd8a
idanhaim <haimdude@gmail.com>
2018-01-04 18:01:56 +0200

    RDBC-100 Fix metadata only query option

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='pyravendb',
     packages=find_packages(),
-    version='4.0.1.9',
+    version='4.0.1.10',
     description='This is the official python client for RavenDB v4.0 document database',
     author='Idan Haim Shalom',
     author_email='haimdude@gmail.com',
