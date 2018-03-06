52b104231540c4e35dae4559d532ce7b1a9ad324
idanhaim <haimdude@gmail.com>
2018-03-06 18:06:04 +0200

    Fix session stream

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='pyravendb',
     packages=find_packages(),
-    version='3.5.4.2',
+    version='3.5.4.3',
     description='This is the official python client for RavenDB document database',
     author='Idan Haim Shalom',
     author_email='haimdude@gmail.com',
