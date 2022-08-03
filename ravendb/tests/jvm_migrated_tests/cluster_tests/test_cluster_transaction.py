2c0fd2db81b39ed4e3563ebbf933ca2500f75a8f
Gracjan Sadowicz <gracjan@ravendb.net>
2022-08-03 12:34:23 +0200

    RDBC-580 ClusterTransactionTest::throwOnInvalidTransactionMode

--- ravendb/tests/jvm_migrated_tests/cluster_tests/test_cluster_transaction.py
+++ ravendb/tests/jvm_migrated_tests/cluster_tests/test_cluster_transaction.py
@@ -27,3 +27,31 @@ class TestClusterTransaction(TestBase):
             self.assertEqual(user1.name, user.name)
             user = session.load("foo/bar", User)
             self.assertEqual(user3.name, user.name)
+
+    def test_throw_on_invalid_transaction_mode(self):
+        user1 = User()
+        user1.name = "Karmel"
+
+        with self.store.open_session() as session:
+            with self.assertRaises(RuntimeError):
+                session.advanced.cluster_transaction.create_compare_exchange_value("usernames/ayende", user1)
+            with self.assertRaises(RuntimeError):
+                session.advanced.cluster_transaction.delete_compare_exchange_value("usernames/ayende", 0)
+
+        options = SessionOptions(transaction_mode=TransactionMode.CLUSTER_WIDE)
+        options.disable_atomic_document_writes_in_cluster_wide_transaction = True
+
+        with self.store.open_session(session_options=options) as session:
+            session.advanced.cluster_transaction.create_compare_exchange_value("usernames/ayende", user1)
+            session.transaction_mode = TransactionMode.SINGLE_NODE
+            with self.assertRaises(RuntimeError):
+                session.save_changes()
+
+            session.transaction_mode = TransactionMode.CLUSTER_WIDE
+            session.save_changes()
+
+            u = session.advanced.cluster_transaction.get_compare_exchange_value("usernames/ayende", User)
+            self.assertEqual(user1.name, u.value.name)
+
+
+
