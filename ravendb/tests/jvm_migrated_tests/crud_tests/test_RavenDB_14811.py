bbe89b7c71d1b7e289085420a5531fa6662fca6e
Gracjan Sadowicz <gracjan@ravendb.net>
2022-08-05 14:25:09 +0200

    Changed RavenDB_14810Test::can_project_id_field

--- ravendb/tests/jvm_migrated_tests/crud_tests/test_RavenDB_14811.py
+++ ravendb/tests/jvm_migrated_tests/crud_tests/test_RavenDB_14811.py
@@ -1,7 +1,5 @@
-import unittest
-
 from ravendb import QueryData
-from ravendb.tests.test_base import TestBase, UserWithId
+from ravendb.tests.test_base import TestBase
 
 
 class User:
@@ -19,7 +17,7 @@ class UserProjectionIntId:
 
 class UserProjectionStringId:
     def __init__(self, name=None, Id=None):
-        self.Id =
+        self.Id = Id
         self.name = name
 
 
@@ -27,23 +25,26 @@ class TestRavenDB14811(TestBase):
     def setUp(self):
         super(TestRavenDB14811, self).setUp()
 
-    @unittest.skip("RDBC-466")
     def test_can_project_id_field_in_class(self):
-        user = UserWithId("Grisha", 34)
+        user = User(name="Grisha", age=34)
         with self.store.open_session() as session:
             session.store(user)
             session.save_changes()
 
         with self.store.open_session() as session:
-            result = session.query(UserWithId).select(UserProjectionIntId(), "name").first()
+            result = session.query(object_type=User).select_fields(UserProjectionIntId, "name").first()
             self.assertIsNotNone(result)
-            self.assertEqual(0, result.Id)
+            self.assertIsNone(None, result.Id)
             self.assertEqual(user.name, result.name)
 
         with self.store.open_session() as session:
-            result = session.query(UserWithId).select(UserProjectionIntId(), ["Id"], ["name"]).first()
+            result = (
+                session.query(object_type=User)
+                .select_fields_query_data(UserProjectionIntId, QueryData(["Id"], ["name"]))
+                .first()
+            )
             self.assertIsNotNone(result)
-            self.assertEqual(0, result.Id)
+            self.assertIsNone(result.Id)
             self.assertEqual(user.Id, result.name)
 
     def test_can_project_id_field(self):
