157f3a174490447829092b67d06b7358cf6956d8
Gracjan Sadowicz <gracjan@ravendb.net>
2022-08-10 13:52:17 +0200

    RDBC-609 ChangesTest::canCanNotificationAboutDocumentsFromCollection

--- ravendb/tests/jvm_migrated_tests/server_tests/documents/notifications/test_changes.py
+++ ravendb/tests/jvm_migrated_tests/server_tests/documents/notifications/test_changes.py
@@ -1,9 +1,10 @@
 from threading import Event
-from typing import Optional
+from typing import Optional, List
 
 from ravendb.changes.observers import ActionObserver
 from ravendb.changes.types import DocumentChange
 from ravendb.infrastructure.entities import User
+from ravendb.infrastructure.orders import Order
 from ravendb.tests.test_base import TestBase
 
 
@@ -48,3 +49,40 @@ class TestChanges(TestBase):
 
         event.wait(1)
         self.assertIsNone(document_change)
+
+    def test_can_can_notification_about_documents_from_collection(self):
+        event = Event()
+        changes_list: List[DocumentChange] = []
+        observable = self.store.changes().for_documents_in_collection("users")
+
+        def __ev(value: DocumentChange):
+            nonlocal changes_list
+            changes_list.append(value)
+            if len(changes_list) == 2:
+                event.set()
+
+        subscription = ActionObserver(__ev)
+        close_action = observable.subscribe(subscription)
+        observable.ensure_subscribe_now()
+
+        with self.store.open_session() as session:
+            session.store(User("users/1"))
+            session.save_changes()
+
+        with self.store.open_session() as session:
+            session.store(Order("orders/1"))
+            session.save_changes()
+
+        with self.store.open_session() as session:
+            session.store(User("users/2"))
+            session.save_changes()
+
+        event.wait(2)
+
+        self.assertIsNotNone(changes_list[0])
+        self.assertEqual(changes_list[0].key, "users/1")
+
+        self.assertIsNotNone(changes_list[1])
+        self.assertEqual(changes_list[1].key, "users/2")
+
+        close_action()
