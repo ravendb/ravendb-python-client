be83b64052d6a6765438d184ffe30188b900f6df
Gracjan Sadowicz <gracjan@ravendb.net>
2022-09-29 13:10:58 +0200

    RDBC-611 SubscriptionsBasicTest::shouldRespectMaxDocCountInBatch

--- ravendb/tests/jvm_migrated_tests/client_tests/subscriptions_tests/test_basic_subscription.py
+++ ravendb/tests/jvm_migrated_tests/client_tests/subscriptions_tests/test_basic_subscription.py
@@ -4,8 +4,10 @@ from ravendb.documents.subscriptions.options import (
     SubscriptionWorkerOptions,
     SubscriptionOpeningStrategy,
 )
+from ravendb.documents.subscriptions.worker import SubscriptionBatch
 from ravendb.exceptions.exceptions import SubscriptionInUseException
 from ravendb.infrastructure.entities import User
+from ravendb.infrastructure.orders import Company
 from ravendb.tests.test_base import TestBase
 
 
@@ -57,6 +59,32 @@ class TestBasicSubscription(TestBase):
                 throwing_subscription_worker.close()
             if not_throwing_subscription_worker is not None:
                 not_throwing_subscription_worker.close()
+
+    def test_should_respect_max_doc_count_in_batch(self):
+        with self.store.open_session() as session:
+            for i in range(100):
+                session.store(Company())
+
+            session.save_changes()
+
+        key = self.store.subscriptions.create_for_class(Company)
+        options = SubscriptionWorkerOptions(key)
+        options.max_docs_per_batch = 25
+
+        with self.store.subscriptions.get_subscription_worker(options) as subscription_worker:
+            semaphore = Semaphore(0)
+            total_items = 0
+
+            def __run(batch: SubscriptionBatch):
+                nonlocal total_items
+                total_items += batch.number_of_items_in_batch
+                self.assertLessEqual(batch.number_of_items_in_batch, 25)
+
+                if total_items == 100:
+                    semaphore.release()
+
+            subscription_worker.run(__run)
+            self.assertTrue(semaphore.acquire(True, self.reasonable_amount_of_time))
 
     def _put_user_doc(self):
         with self.store.open_session() as session:
