19574851c563020c12828bdb32f77d583a92f861
Gracjan Sadowicz <gracjan@ravendb.net>
2023-03-14 12:52:11 +0100

    RDBC-680 RavenDB_15080Test::counterOperationsShouldBeCaseInsensitiveToCounterName

--- ravendb/tests/jvm_migrated_tests/issues_tests/test_ravenDB_15080.py
+++ ravendb/tests/jvm_migrated_tests/issues_tests/test_ravenDB_15080.py
@@ -113,3 +113,73 @@ class TestRavenDB15080(TestBase):
             counters = session.advanced.get_counters_for(company)
             self.assertEqual(1, len(counters))
             self.assertIn("Cats", counters)
+
+    def test_counter_operations_should_be_case_insensitive_to_counter_name(self):
+        with self.store.open_session() as session:
+            user = User("Aviv")
+            session.store(user, "users/1")
+
+            session.counters_for("users/1").increment("abc")
+            session.save_changes()
+
+        with self.store.open_session() as session:
+            # should NOT create a new counter
+            session.counters_for("users/1").increment("ABc")
+            session.save_changes()
+
+        with self.store.open_session() as session:
+            self.assertEqual(1, len(session.counters_for("users/1").get_all()))
+
+        with self.store.open_session() as session:
+            # get should be case-insensitive to counter name
+
+            val = session.counters_for("users/1").get("AbC")
+            self.assertEqual(2, val)
+
+            doc = session.load("users/1", User)
+            counter_names = session.advanced.get_counters_for(doc)
+            self.assertEqual(1, len(counter_names))
+            self.assertEqual("abc", counter_names[0])  # metadata counter-names should preserve their original casing
+
+        with self.store.open_session() as session:
+            session.counters_for("users/1").increment("XyZ")
+            session.save_changes()
+
+        with self.store.open_session() as session:
+            val = session.counters_for("users/1").get("xyz")
+            self.assertEqual(1, val)
+
+            doc = session.load("users/1", User)
+            counter_names = session.advanced.get_counters_for(doc)
+            self.assertEqual(2, len(counter_names))
+
+            # metadata counter-names should preserve their original casing
+            self.assertEqual("abc", counter_names[0])
+            self.assertEqual("XyZ", counter_names[1])
+
+        with self.store.open_session() as session:
+            # delete should be case-insensitive to counter name
+
+            session.counters_for("users/1").delete("aBC")
+            session.save_changes()
+
+        with self.store.open_session() as session:
+            val = session.counters_for("users/1").get("abc")
+            self.assertIsNone(val)
+
+            doc = session.load("users/1", User)
+            counter_names = session.advanced.get_counters_for(doc)
+            self.assertEqual(1, len(counter_names))
+            self.assertIn("XyZ", counter_names)
+
+        with self.store.open_session() as session:
+            session.counters_for("users/1").delete("xyZ")
+            session.save_changes()
+
+        with self.store.open_session() as session:
+            val = session.counters_for("users/1").get("Xyz")
+            self.assertIsNone(val)
+
+            doc = session.load("users/1", User)
+            counter_names = session.advanced.get_counters_for(doc)
+            self.assertIsNone(counter_names)
