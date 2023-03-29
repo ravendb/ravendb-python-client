c130c084a6394eca63f1295a4917fcb2c60fa93d
Gracjan Sadowicz <gracjan@ravendb.net>
2023-03-29 12:49:21 +0200

    RDBC-685 RavenDB_11703Test::canGetNotificationAboutCounterDelete

--- ravendb/tests/jvm_migrated_tests/issues_tests/test_ravenDB_11703.py
+++ ravendb/tests/jvm_migrated_tests/issues_tests/test_ravenDB_11703.py
@@ -47,3 +47,40 @@ class TestRavenDB11703(TestBase):
         self.assertIsNotNone(counter_change.change_vector)
 
         close_method()
+
+    def test_can_get_notification_about_counter_delete(self):
+        changes_queue = Queue()
+        observer = self.store.changes().for_counters_of_document("users/1")
+        close_method = observer.subscribe(changes_queue.put)
+
+        with self.store.open_session() as session:
+            session.store(User(), "users/1")
+            session.save_changes()
+
+        with self.store.open_session() as session:
+            session.counters_for("users/1").increment("likes")
+            session.save_changes()
+
+        counter_change: CounterChange = changes_queue.get(timeout=2)
+        self.assertIsNotNone(counter_change)
+
+        self.assertEqual("users/1", counter_change.document_id)
+        self.assertEqual("Users", counter_change.collection_name)
+        self.assertEqual(CounterChangeTypes.PUT, counter_change.type_of_change)
+        self.assertEqual("likes", counter_change.name)
+        self.assertEqual(1, counter_change.value)
+        self.assertIsNotNone(counter_change.change_vector)
+
+        with self.store.open_session() as session:
+            session.counters_for("users/1").delete("likes")
+            session.save_changes()
+
+        counter_change = changes_queue.get(timeout=2)
+        self.assertIsNotNone(counter_change)
+
+        self.assertEqual("users/1", counter_change.document_id)
+        self.assertEqual(CounterChangeTypes.DELETE, counter_change.type_of_change)
+        self.assertEqual("likes", counter_change.name)
+        self.assertEqual(0, counter_change.value)
+        self.assertIsNotNone(counter_change.change_vector)
+        close_method()
