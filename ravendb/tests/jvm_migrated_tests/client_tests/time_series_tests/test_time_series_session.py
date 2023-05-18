93c0f119780787fe3201dbb1edc4652ddd89e980
Gracjan Sadowicz <gracjan@ravendb.net>
2023-05-18 15:03:35 +0200

    RDBC-697 TimeSeriesSessionTest::canCreateSimpleTimeSeries2

--- ravendb/tests/jvm_migrated_tests/client_tests/time_series_tests/test_time_series_session.py
+++ ravendb/tests/jvm_migrated_tests/client_tests/time_series_tests/test_time_series_session.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime, timedelta
 
 from ravendb.infrastructure.entities import User
 from ravendb.tests.test_base import TestBase
@@ -9,13 +9,13 @@ class TestTimeSeriesSession(TestBase):
         super(TestTimeSeriesSession, self).setUp()
 
     def test_can_create_simple_time_series(self):
-        today = datetime.datetime.today()
-        base_line = datetime.datetime(today.year, today.month, today.day)
+        today = datetime.today()
+        base_line = datetime(today.year, today.month, today.day)
 
         with self.store.open_session() as session:
             user = User(name="Oren")
             session.store(user, "users/ayende")
-            timestamp = base_line + datetime.timedelta(minutes=1)
+            timestamp = base_line + timedelta(minutes=1)
             session.time_series_for("users/ayende", "Heartrate").append(timestamp, 59, "watches/fitbit")
             session.save_changes()
 
@@ -24,3 +24,22 @@ class TestTimeSeriesSession(TestBase):
             self.assertEqual([59], val.values)
             self.assertEqual("watches/fitbit", val.tag)
             self.assertEqual(timestamp, val.timestamp)
+
+    def test_can_crate_simple_time_series_2(self):
+        today = datetime.today()
+        base_line = datetime(today.year, today.month, today.day)
+
+        with self.store.open_session() as session:
+            user = User(name="Oren")
+            session.store(user, "users/ayende")
+
+            tsf = session.time_series_for("users/ayende", "Heartrate")
+            tsf.append(base_line + timedelta(1), 59, "watches/fitbit")
+            tsf.append(base_line + timedelta(2), 60, "watches/fitbit")
+            tsf.append(base_line + timedelta(3), 61, "watches/fitbit")
+
+            session.save_changes()
+
+        with self.store.open_session() as session:
+            val = session.time_series_for("users/ayende", "Heartrate").get()
+            self.assertEqual(3, len(val))
