bcc54eb6c21d434d43f707b0da9d19ceca83cb2f
Gracjan Sadowicz <gracjan@ravendb.net>
2023-11-08 16:03:47 +0100

    RDBC-698 test_should_get_time_series_value_from_cache

--- ravendb/tests/jvm_migrated_tests/client_tests/time_series_tests/test_time_series_ranges_cache.py
+++ ravendb/tests/jvm_migrated_tests/client_tests/time_series_tests/test_time_series_ranges_cache.py
@@ -45,3 +45,31 @@ class TestTimeSeriesRangesCache(TestBase):
             ranges = cache.get(ts_name)
             self.assertIsNotNone(ranges)
             self.assertEqual(1, len(ranges))
+
+    def test_should_get_time_series_value_from_cache(self):
+        base_line = datetime(2023, 8, 20, 21, 30)
+        doc_id = "users/ayende"
+        ts_name = "Heartrate"
+
+        with self.store.open_session() as session:
+            user = User(name="Oren")
+            session.store(user, doc_id)
+            session.time_series_for(doc_id, ts_name).append(base_line + timedelta(minutes=1), [59], "watches/fitbit")
+            session.save_changes()
+
+        with self.store.open_session() as session:
+            val = session.time_series_for("users/ayende", ts_name).get()[0]
+
+            self.assertEqual([59], val.values)
+            self.assertEqual("watches/fitbit", val.tag)
+            self.assertEqual(base_line + timedelta(minutes=1), val.timestamp)
+
+            self.assertEqual(1, session.advanced.number_of_requests)
+
+            # should load from cache
+            val = session.time_series_for(doc_id, ts_name).get()[0]
+            self.assertEqual([59], val.values)
+            self.assertEqual("watches/fitbit", val.tag)
+            self.assertEqual(base_line + timedelta(minutes=1), val.timestamp)
+
+            self.assertEqual(1, session.advanced.number_of_requests)
