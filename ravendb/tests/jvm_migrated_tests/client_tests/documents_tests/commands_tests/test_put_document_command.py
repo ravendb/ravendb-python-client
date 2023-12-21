c2c8d5ee86da6471114bbb47d2f72e48ca776611
Gracjan Sadowicz <gracjan@ravendb.net>
2023-12-21 15:41:14 +0100

    RDBC-768 PutDocumentCommandTest::canPutDocumentUsingCommand

--- ravendb/tests/jvm_migrated_tests/client_tests/documents_tests/commands_tests/test_put_document_command.py
+++ ravendb/tests/jvm_migrated_tests/client_tests/documents_tests/commands_tests/test_put_document_command.py
@@ -8,6 +8,23 @@ class TestPutDocumentCommand(TestBase):
     def setUp(self):
         super().setUp()
 
+    def test_can_put_document_using_command(self):
+        with self.store.open_session() as session:
+            user = User(name="Gracjan", age=30)
+            node = Utils.entity_to_dict(user, self.store.conventions.json_default_method)
+            command = PutDocumentCommand("users/1", None, node)
+            self.store.get_request_executor().execute_command(command)
+
+            result = command.result
+
+            self.assertEqual("users/1", result.key)
+
+            self.assertIsNotNone(result.change_vector)
+
+            with self.store.open_session() as session:
+                loaded_user = session.load("users/1", User)
+                self.assertEqual(loaded_user.name, "Gracjan")
+
     def test_can_put_document_using_command_with_surrogate_pairs(self):
         name_with_emojis = "Gracjan \uD83D\uDE21\uD83D\uDE21\uD83E\uDD2C\uD83D\uDE00😡😡🤬😀"
 
