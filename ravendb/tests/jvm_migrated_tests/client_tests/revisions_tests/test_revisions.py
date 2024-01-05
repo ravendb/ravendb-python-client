a9e74cd7d047f1fd366ab742a35d89f52db309ac
Gracjan Sadowicz <gracjan@ravendb.net>
2024-01-05 16:02:27 +0100

    RDBC-775 fix flaky test - created a ticket https://issues.hibernatingrhinos.com/issue/RDBC-778

--- ravendb/tests/jvm_migrated_tests/client_tests/revisions_tests/test_revisions.py
+++ ravendb/tests/jvm_migrated_tests/client_tests/revisions_tests/test_revisions.py
@@ -23,40 +23,27 @@ class TestRevisions(TestBase):
                 session.store(user, "users/1")
                 session.save_changes()
 
-        sleep(2)
         with self.store.open_session() as session:
             all_revisions = session.advanced.revisions.get_for("users/1", User)
             self.assertEqual(4, len(all_revisions))
             self.assertEqual(["user4", "user3", "user2", "user1"], [x.name for x in all_revisions])
 
-            sleep(2)
             revisions_skip_first = session.advanced.revisions.get_for("users/1", User, 1)
             self.assertEqual(3, len(revisions_skip_first))
             self.assertEqual(["user3", "user2", "user1"], [x.name for x in revisions_skip_first])
 
-            sleep(2)
             revisions_skip_first_take_two = session.advanced.revisions.get_for("users/1", User, 1, 2)
             self.assertEqual(2, len(revisions_skip_first_take_two))
             self.assertEqual(["user3", "user2"], [x.name for x in revisions_skip_first_take_two])
 
-            sleep(2)
             all_metadata = session.advanced.revisions.get_metadata_for("users/1")
             self.assertEqual(4, len(all_metadata))
 
-            sleep(2)
             metadata_skip_first = session.advanced.revisions.get_metadata_for("users/1", 1)
             self.assertEqual(3, len(metadata_skip_first))
 
-            sleep(2)
             metadata_skip_first_take_two = session.advanced.revisions.get_metadata_for("users/1", 1, 2)
             self.assertEqual(2, len(metadata_skip_first_take_two))
-
-            sleep(2)
-
-            user = session.advanced.revisions.get_by_change_vector(
-                metadata_skip_first[0].metadata.get(constants.Documents.Metadata.CHANGE_VECTOR), User
-            )
-            self.assertEqual("user3", user.name)
 
     def test_can_get_revisions_by_change_vector(self):
         id_ = "users/1"
