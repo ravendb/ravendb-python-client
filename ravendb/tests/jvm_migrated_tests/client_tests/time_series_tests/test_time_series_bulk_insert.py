4ea5b360a26db78491c5f7ba6ca405a9e2e4f5ac
Gracjan Sadowicz <gracjan@ravendb.net>
2024-01-09 16:04:58 +0100

    RDBC-769 TimeSeriesBulkInsertTest::errorHandling

--- ravendb/tests/jvm_migrated_tests/client_tests/time_series_tests/test_time_series_bulk_insert.py
+++ ravendb/tests/jvm_migrated_tests/client_tests/time_series_tests/test_time_series_bulk_insert.py
@@ -221,3 +221,28 @@ class TestTimeSeriesBulkInsert(TestBase):
             self.assertEqual([69], vals[2].values)
             self.assertEqual("watches/fitbit", vals[2].tag)
             self.assertEqual(base_line + timedelta(minutes=3), vals[2].timestamp)
+
+    def test_error_handling(self):
+        base_line = RavenTestHelper.utc_this_month()
+        document_id = "users/ayende"
+
+        with self.store.bulk_insert() as bulk_insert:
+            user = User(name="Oren")
+            bulk_insert.store_as(user, document_id)
+
+            with bulk_insert.time_series_for(document_id, "Heartrate") as time_series_bulk_insert:
+                time_series_bulk_insert.append_single(base_line + timedelta(minutes=1), 59, "watches/fitbit")
+
+                error_message = "There is an already running time series operation, did you forget to close it?"
+
+                self.assertRaisesWithMessageContaining(
+                    bulk_insert.store, RuntimeError, error_message, User(name="Oren")
+                )
+
+                self.assertRaisesWithMessageContaining(
+                    bulk_insert.counters_for("test").increment, RuntimeError, error_message, "1", 1
+                )
+
+                self.assertRaisesWithMessageContaining(
+                    bulk_insert.time_series_for, RuntimeError, error_message, document_id, "Heartrate"
+                )
