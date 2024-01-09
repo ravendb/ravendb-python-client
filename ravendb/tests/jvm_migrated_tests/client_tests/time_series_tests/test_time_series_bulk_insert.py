2f3b0498a144eb694f39abf80b40e00f31543211
Gracjan Sadowicz <gracjan@ravendb.net>
2024-01-09 14:18:36 +0100

    RDBC-769 TimeSeriesBulkInsertTest::usingDifferentNumberOfValues_SmallToLarge

--- ravendb/tests/jvm_migrated_tests/client_tests/time_series_tests/test_time_series_bulk_insert.py
+++ ravendb/tests/jvm_migrated_tests/client_tests/time_series_tests/test_time_series_bulk_insert.py
@@ -152,7 +152,9 @@ class TestTimeSeriesBulkInsert(TestBase):
             with self.store.bulk_insert() as bulk_insert:
                 with bulk_insert.time_series_for(document_id, "Heartrate") as time_series_bulk_insert:
                     for j in range(1000):
-                        time_series_bulk_insert.append_single(base_line + timedelta(minutes=offset), offset + 1, "watches/fitbit")
+                        time_series_bulk_insert.append_single(
+                            base_line + timedelta(minutes=offset), offset + 1, "watches/fitbit"
+                        )
                         offset += 1
 
         offset = 0
@@ -161,7 +163,9 @@ class TestTimeSeriesBulkInsert(TestBase):
             with self.store.bulk_insert() as bulk_insert:
                 with bulk_insert.time_series_for(document_id, "Pulse") as time_series_bulk_insert:
                     for j in range(1000):
-                        time_series_bulk_insert.append_single(base_line + timedelta(minutes=offset), offset + 1, "watches/fitbit")
+                        time_series_bulk_insert.append_single(
+                            base_line + timedelta(minutes=offset), offset + 1, "watches/fitbit"
+                        )
                         offset += 1
 
         with self.store.open_session() as session:
@@ -189,4 +193,31 @@ class TestTimeSeriesBulkInsert(TestBase):
             self.assertEqual("Heartrate", ts_names[0])
             self.assertEqual("Pulse", ts_names[1])
 
+    def test_using_different_number_of_values_small_to_large(self):
+        base_line = RavenTestHelper.utc_this_month()
+        document_id = "users/ayende"
 
+        with self.store.bulk_insert() as bulk_insert:
+            user = User(name="Oren")
+            bulk_insert.store_as(user, document_id)
+
+            with bulk_insert.time_series_for(document_id, "Heartrate") as time_series_bulk_insert:
+                time_series_bulk_insert.append_single(base_line + timedelta(minutes=1), 59, "watches/fitbit")
+                time_series_bulk_insert.append(base_line + timedelta(minutes=2), [70, 120, 80], "watches/apple")
+                time_series_bulk_insert.append_single(base_line + timedelta(minutes=3), 69, "watches/fitbit")
+
+        with self.store.open_session() as session:
+            vals = session.time_series_for("users/ayende", "Heartrate").get(None, None)
+
+            self.assertEqual(3, len(vals))
+            self.assertEqual([59], vals[0].values)
+            self.assertEqual("watches/fitbit", vals[0].tag)
+            self.assertEqual(base_line + timedelta(minutes=1), vals[0].timestamp)
+
+            self.assertEqual([70, 120, 80], vals[1].values)
+            self.assertEqual("watches/apple", vals[1].tag)
+            self.assertEqual(base_line + timedelta(minutes=2), vals[1].timestamp)
+
+            self.assertEqual([69], vals[2].values)
+            self.assertEqual("watches/fitbit", vals[2].tag)
+            self.assertEqual(base_line + timedelta(minutes=3), vals[2].timestamp)
