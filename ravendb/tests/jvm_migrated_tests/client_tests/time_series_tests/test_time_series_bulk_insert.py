133e36bd7ae8ab6a222a150002ded16f90b44f32
Gracjan Sadowicz <gracjan@ravendb.net>
2024-01-09 12:40:53 +0100

    RDBC-769 TimeSeriesBulkInsertTest::canRequestNonExistingTimeSeriesRange

--- ravendb/tests/jvm_migrated_tests/client_tests/time_series_tests/test_time_series_bulk_insert.py
+++ ravendb/tests/jvm_migrated_tests/client_tests/time_series_tests/test_time_series_bulk_insert.py
@@ -1,3 +1,4 @@
+import uuid
 from datetime import timedelta
 
 from ravendb.infrastructure.entities import User
@@ -35,3 +36,28 @@ class TestTimeSeriesBulkInsert(TestBase):
 
             vals = session.time_series_for(document_id, "Heartrate2").get(None, None)
             self.assertIsNone(vals)
+
+    def test_can_request_non_existing_time_series_range(self):
+        base_line = RavenTestHelper.utc_this_month()
+        document_id = "users/ayende"
+
+        with self.store.bulk_insert() as bulk_insert:
+            user = User(name="Oren")
+            bulk_insert.store_as(user, document_id)
+
+            with bulk_insert.time_series_for(document_id, "Heartrate") as time_series_bulk_insert:
+                time_series_bulk_insert.append_single(base_line, 58, "watches/fitbit")
+                time_series_bulk_insert.append_single(base_line + timedelta(minutes=10), 60, "watches/fitbit")
+
+        with self.store.open_session() as session:
+            vals = session.time_series_for("users/ayende", "Heartrate").get(
+                base_line - timedelta(minutes=10), base_line - timedelta(minutes=5)
+            )
+
+            self.assertEqual(0, len(vals))
+
+            vals = session.time_series_for("users/ayende", "Heartrate").get(
+                base_line + timedelta(minutes=5), base_line + timedelta(minutes=9)
+            )
+
+            self.assertEqual(0, len(vals))
