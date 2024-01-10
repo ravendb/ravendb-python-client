3ca05836e1751dec949d9398ea0250ba940de3cc
Gracjan Sadowicz <gracjan@ravendb.net>
2024-01-10 13:46:21 +0100

    RDBC-769 TypedBulkInsert::usingDifferentTags

--- ravendb/tests/jvm_migrated_tests/client_tests/time_series_tests/test_typed_bulk_insert.py
+++ ravendb/tests/jvm_migrated_tests/client_tests/time_series_tests/test_typed_bulk_insert.py
@@ -48,3 +48,28 @@ class TestTypedBulkInsert(TestBase):
             self.assertEqual(62, vals[2].value.heart_rate)
             self.assertEqual("watches/apple-watch", vals[2].tag)
             self.assertEqual(base_line + timedelta(minutes=3), vals[2].timestamp)
+
+    def test_using_different_tags(self):
+        base_line = RavenTestHelper.utc_today()
+        document_id = "users/ayende"
+
+        with self.store.bulk_insert() as bulk_insert:
+            user = User(name="Oren")
+            bulk_insert.store_as(user, document_id)
+
+            with bulk_insert.typed_time_series_for(HeartRateMeasure, document_id, "heartrate") as ts:
+                ts.append_single(base_line + timedelta(minutes=1), HeartRateMeasure(59), "watches/fitbit")
+                ts.append_single(base_line + timedelta(minutes=2), HeartRateMeasure(70), "watches/apple-watch")
+
+        with self.store.open_session() as session:
+            vals = session.time_series_for(document_id, "heartrate").get()
+
+            self.assertEqual(2, len(vals))
+
+            self.assertEqual([59], vals[0].values)
+            self.assertEqual("watches/fitbit", vals[0].tag)
+            self.assertEqual(base_line + timedelta(minutes=1), vals[0].timestamp)
+
+            self.assertEqual([70], vals[1].values)
+            self.assertEqual("watches/apple-watch", vals[1].tag)
+            self.assertEqual(base_line + timedelta(minutes=2), vals[1].timestamp)
