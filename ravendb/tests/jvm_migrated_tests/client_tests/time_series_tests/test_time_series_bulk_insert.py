1c2ef5b70e1e7c9be09135b2bab883523ed08d60
Gracjan Sadowicz <gracjan@ravendb.net>
2024-01-10 11:44:42 +0100

    RDBC-769 TimeSeriesBulkInsertTest::canGetTimeSeriesNames

--- ravendb/tests/jvm_migrated_tests/client_tests/time_series_tests/test_time_series_bulk_insert.py
+++ ravendb/tests/jvm_migrated_tests/client_tests/time_series_tests/test_time_series_bulk_insert.py
@@ -1,5 +1,5 @@
-import uuid
-from datetime import timedelta
+import time
+from datetime import timedelta, datetime
 from typing import List
 
 from ravendb.documents.session.time_series import TimeSeriesEntry
@@ -391,3 +391,59 @@ class TestTimeSeriesBulkInsert(TestBase):
             for i in range(10000):
                 self.assertEqual(base_line + timedelta(minutes=i), vals[i].timestamp)
                 self.assertEqual(1 + i, vals[i].values[0])
+
+    def test_can_get_time_series_names(self):
+        base_line = RavenTestHelper.utc_this_month()
+        document_id_1 = "users/karmel"
+        document_id_2 = "users/ayende"
+
+        with self.store.bulk_insert() as bulk_insert:
+            user = User()
+            bulk_insert.store_as(user, document_id_1)
+            with bulk_insert.time_series_for(document_id_1, "Nasdaq2") as time_series_bulk_insert:
+                time_series_bulk_insert.append_single(datetime.utcnow(), 7547.31, "web")
+
+        with self.store.bulk_insert() as bulk_insert:
+            with bulk_insert.time_series_for(document_id_1, "Heartrate2") as time_series_bulk_insert2:
+                time_series_bulk_insert2.append_single(datetime.utcnow(), 7547.31, "web")
+
+        with self.store.bulk_insert() as bulk_insert:
+            user = User()
+            bulk_insert.store_as(user, document_id_2)
+            with bulk_insert.time_series_for(document_id_2, "Nasdaq") as time_series_bulk_insert:
+                time_series_bulk_insert.append_single(datetime.utcnow(), 7547.31, "web")
+
+        with self.store.bulk_insert() as bulk_insert:
+            with bulk_insert.time_series_for(document_id_2, "Heartrate") as time_series_bulk_insert:
+                time_series_bulk_insert.append_single(datetime.utcnow(), 58, "fitbit")
+
+        with self.store.open_session() as session:
+            user = session.load(document_id_2, User)
+            ts_names = session.advanced.get_time_series_for(user)
+            self.assertEqual(2, len(ts_names))
+
+            # should be sorted
+            self.assertEqual("Heartrate", ts_names[0])
+            self.assertEqual("Nasdaq", ts_names[1])
+
+        with self.store.open_session() as session:
+            user = session.load(document_id_1, User)
+            ts_names = session.advanced.get_time_series_for(user)
+            self.assertEqual(2, len(ts_names))
+
+            # should be sorted
+            self.assertEqual("Heartrate2", ts_names[0])
+            self.assertEqual("Nasdaq2", ts_names[1])
+
+        with self.store.bulk_insert() as bulk_insert:
+            with bulk_insert.time_series_for(document_id_2, "heartrate") as time_series_bulk_insert:
+                time_series_bulk_insert.append_single(base_line + timedelta(minutes=1), 58, "fitbit")
+
+        with self.store.open_session() as session:
+            user = session.load("users/ayende", User)
+            ts_names = session.advanced.get_time_series_for(user)
+            self.assertEqual(2, len(ts_names))
+
+            # should preserve original casing
+            self.assertEqual("Heartrate", ts_names[0])
+            self.assertEqual("Nasdaq", ts_names[1])
