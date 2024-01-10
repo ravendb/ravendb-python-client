30c6abae356b73a121275b0b047dd9b5c1e19d56
Gracjan Sadowicz <gracjan@ravendb.net>
2024-01-10 11:59:38 +0100

    RDBC-769 TimeSeriesBulkInsertTest::canSkipAndTakeTimeSeries

--- ravendb/tests/jvm_migrated_tests/client_tests/time_series_tests/test_time_series_bulk_insert.py
+++ ravendb/tests/jvm_migrated_tests/client_tests/time_series_tests/test_time_series_bulk_insert.py
@@ -447,3 +447,26 @@ class TestTimeSeriesBulkInsert(TestBase):
             # should preserve original casing
             self.assertEqual("Heartrate", ts_names[0])
             self.assertEqual("Nasdaq", ts_names[1])
+
+    def test_can_skip_take_time_series(self):
+        base_line = RavenTestHelper.utc_this_month()
+        document_id = "users/ayende"
+
+        with self.store.bulk_insert() as bulk_insert:
+            user = User()
+            user.name = "Oren"
+            bulk_insert.store_as(user, document_id)
+
+            with bulk_insert.time_series_for(document_id, "Heartrate") as time_series_bulk_insert:
+                for i in range(100):
+                    time_series_bulk_insert.append_single(base_line + timedelta(minutes=i), 100 + i, "watches/fitbit")
+                    i += 1
+
+        with self.store.open_session() as session:
+            vals = session.time_series_for("users/ayende", "Heartrate").get(None, None, 5, 20)
+
+            self.assertEqual(20, len(vals))
+
+            for i in range(len(vals)):
+                self.assertEqual(base_line + timedelta(minutes=5 + i), vals[i].timestamp)
+                self.assertEqual(105 + i, vals[i].value)
