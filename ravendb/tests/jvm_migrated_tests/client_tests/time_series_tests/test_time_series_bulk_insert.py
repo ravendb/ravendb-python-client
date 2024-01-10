052a69d2106dcdac538ec7bd1f8e117707eca5b6
Gracjan Sadowicz <gracjan@ravendb.net>
2024-01-10 11:19:13 +0100

    RDBC-769 TimeSeriesBulkInsertTest::usingDifferentNumberOfValues_LargeToSmall

--- ravendb/tests/jvm_migrated_tests/client_tests/time_series_tests/test_time_series_bulk_insert.py
+++ ravendb/tests/jvm_migrated_tests/client_tests/time_series_tests/test_time_series_bulk_insert.py
@@ -333,3 +333,33 @@ class TestTimeSeriesBulkInsert(TestBase):
             self.assertEqual(1, len(ts_names))
 
             self.assertEqual("Heartrate", ts_names[0])
+
+    def test_using_different_number_of_values_large_to_small(self):
+        base_line = RavenTestHelper.utc_this_month()
+        document_id = "users/ayende"
+
+        with self.store.bulk_insert() as bulk_insert:
+            user = User(name="Oren")
+            bulk_insert.store_as(user, document_id)
+
+            with bulk_insert.time_series_for(document_id, "Heartrate") as time_series_bulk_insert:
+                time_series_bulk_insert.append(base_line + timedelta(minutes=1), [70, 120, 80], "watches/apple")
+                time_series_bulk_insert.append_single(base_line + timedelta(minutes=2), 59, "watches/fitbit")
+                time_series_bulk_insert.append_single(base_line + timedelta(minutes=3), 69, "watches/fitbit")
+
+        with self.store.open_session() as session:
+            vals = session.time_series_for("users/ayende", "Heartrate").get(None, None)
+
+            self.assertEqual(3, len(vals))
+
+            self.assertEqual([70, 120, 80], vals[0].values)
+            self.assertEqual("watches/apple", vals[0].tag)
+            self.assertEqual(base_line + timedelta(minutes=1), vals[0].timestamp)
+
+            self.assertEqual([59], vals[1].values)
+            self.assertEqual("watches/fitbit", vals[1].tag)
+            self.assertEqual(base_line + timedelta(minutes=2), vals[1].timestamp)
+
+            self.assertEqual([69], vals[2].values)
+            self.assertEqual("watches/fitbit", vals[2].tag)
+            self.assertEqual(base_line + timedelta(minutes=3), vals[2].timestamp)
