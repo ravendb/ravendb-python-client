631c2e0844fd0a75b6b6aaf1432ebe84e1cfa66b
Gracjan Sadowicz <gracjan@ravendb.net>
2024-02-08 16:26:50 +0100

    RDBC-796 Release 5.2.5 client to solve dependency conflicts

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="ravendb",
     packages=find_packages(exclude=["*.tests.*", "tests", "*.tests", "tests.*"]),
-    version="5.2.4",
+    version="5.2.5",
     long_description_content_type="text/markdown",
     long_description=open("README_pypi.md").read(),
     description="Python client for RavenDB NoSQL Database",
@@ -21,7 +21,7 @@ setup(
         "requests >= 2.27.1",
         "requests-pkcs12 >= 1.13",
         "pyOpenSSL >= 22.0.0",
-        "ijson >= 3",
+        "ijson ~= 3.2.3",
         "websocket-client >= 0.46.0",
         "inflect >= 5.4.0",
     ],
