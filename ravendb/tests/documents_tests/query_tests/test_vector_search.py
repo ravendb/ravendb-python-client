73b894ee8ec0278087075dc9d7f40d9e62ca9b46
Gracjan Sadowicz <gracjan@ravendb.net>
2026-01-30 17:01:02 +0100

    RDBC-1013 Replace deprecated API usage in tests

--- ravendb/tests/documents_tests/query_tests/test_vector_search.py
+++ ravendb/tests/documents_tests/query_tests/test_vector_search.py
@@ -1,4 +1,6 @@
 import unittest
+
+from ravendb.documents.indexes.vector.embedding import VectorEmbeddingType
 from ravendb.documents.queries.vector import VectorQuantizer
 from ravendb.tests.dotnet_migrated_tests.test_ravenDB_22076 import Dto
 from ravendb.tests.test_base import TestBase
@@ -126,46 +128,46 @@ class TestVectorQuantizer(unittest.TestCase):
 class TestVectorSearch(TestBase):
     def test_should_generate_rql_with_text_field_using_named_ai_task(self):
         with self.store.open_session() as session:
-            q = session.query(object_type=Dto).vector_search_text_using_task("EmbeddingField", "fishing", "my-ai-task")
+            q = session.query(object_type=Dto).vector_search_text("EmbeddingField", "fishing", embedding_generation_task_identifier="my-ai-task")
             self.assertEqual(
                 "from 'Dtoes' where vector.search(embedding.text(EmbeddingField, ai.task('my-ai-task')), $p0)",
                 q._to_string(),
             )
 
-            q_exact = session.query(object_type=Dto).vector_search_text_using_task(
-                "EmbeddingField", "fishing", "my-ai-task", is_exact=True
+            q_exact = session.query(object_type=Dto).vector_search_text(
+                "EmbeddingField", "fishing", embedding_generation_task_identifier="my-ai-task", is_exact=True
             )
             self.assertEqual(
                 "from 'Dtoes' where exact(vector.search(embedding.text(EmbeddingField, ai.task('my-ai-task')), $p0))",
                 q_exact._to_string(),
             )
 
-            q2 = session.query(object_type=Dto).vector_search_text_i1_using_task(
-                "EmbeddingField", "fishing", "my-ai-task"
+            q2 = session.query(object_type=Dto).vector_search_text(
+                "EmbeddingField", "fishing", target_quantization=VectorEmbeddingType.BINARY, embedding_generation_task_identifier="my-ai-task"
             )
             self.assertEqual(
                 "from 'Dtoes' where vector.search(embedding.text_i1(EmbeddingField, ai.task('my-ai-task')), $p0)",
                 q2._to_string(),
             )
 
-            q2_exact = session.query(object_type=Dto).vector_search_text_i1_using_task(
-                "EmbeddingField", "fishing", "my-ai-task", is_exact=True
+            q2_exact = session.query(object_type=Dto).vector_search_text(
+                "EmbeddingField", "fishing", target_quantization=VectorEmbeddingType.BINARY, embedding_generation_task_identifier="my-ai-task", is_exact=True
             )
             self.assertEqual(
                 "from 'Dtoes' where exact(vector.search(embedding.text_i1(EmbeddingField, ai.task('my-ai-task')), $p0))",
                 q2_exact._to_string(),
             )
 
-            q3 = session.query(object_type=Dto).vector_search_text_i8_using_task(
-                "EmbeddingField", "fishing", "my-ai-task"
+            q3 = session.query(object_type=Dto).vector_search_text(
+                "EmbeddingField", "fishing", target_quantization=VectorEmbeddingType.INT8, embedding_generation_task_identifier="my-ai-task",
             )
             self.assertEqual(
                 "from 'Dtoes' where vector.search(embedding.text_i8(EmbeddingField, ai.task('my-ai-task')), $p0)",
                 q3._to_string(),
             )
 
-            q3_exact = session.query(object_type=Dto).vector_search_text_i8_using_task(
-                "EmbeddingField", "fishing", "my-ai-task", is_exact=True
+            q3_exact = session.query(object_type=Dto).vector_search_text(
+                "EmbeddingField", "fishing", embedding_generation_task_identifier="my-ai-task", is_exact=True, target_quantization=VectorEmbeddingType.INT8,
             )
             self.assertEqual(
                 "from 'Dtoes' where exact(vector.search(embedding.text_i8(EmbeddingField, ai.task('my-ai-task')), $p0))",
