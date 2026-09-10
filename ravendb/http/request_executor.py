32675c28236ae543ee3a7f23375dc8d15777274f
Gracjan Sadowicz <gracjan@ravendb.net>
2026-09-10 13:06:32 +0200

    Track the 7.2.6 client version and its CDC slot-naming note

--- ravendb/http/request_executor.py
+++ ravendb/http/request_executor.py
@@ -56,7 +56,7 @@ if TYPE_CHECKING:
 class RequestExecutor:
     __INITIAL_TOPOLOGY_ETAG = -2
     __GLOBAL_APPLICATION_IDENTIFIER = uuid.uuid4()
-    CLIENT_VERSION = "7.2.5"
+    CLIENT_VERSION = "7.2.6"
     logger = logging.getLogger("request_executor")
 
     # todo: initializer should take also cryptography certificates
